40274a109455cb4a0c4a90733359a0ade4ff5be4
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-06 16:17:27 -0700

    Created App component and Update index.js since ReactDom.render is deprecated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' // Used rafce, React Arrow Function Export Component
+
+// import { Article, Brand, CTA } from './components'
+
+const App = () => {
+  return (
+    <div>
+        <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App // Export to be able to import the App component in the index.js
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom/client' // Allows us to hook are React application to our index.html file
+import App from './App'
+
+
+const root = ReactDom.createRoot(document.getElementById("root")) // Get the div element from index.html
+
+// Render the App component inside the root div located at the index.html file
+root.render(
+    <App/>
+)
