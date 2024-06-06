fda60b073c5b2c166e7801d37b23ffd4d00b7cf3
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-06 16:15:29 -0700

    Created components and containers needed for the website

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react' // JSX is a syntax extension for JavaScript that lets you write HTML-like markup inside a JavaScript file.
+import './article.css'
+
+const Article = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
