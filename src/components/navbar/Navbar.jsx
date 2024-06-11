8c1d2cf7bb84f96e3c01debf0af4da3ed20b5394
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-10 17:37:14 -0700

    Finished implementing the navbar and added mobile responsiveness for menu to appear

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,20 +37,24 @@ const Navbar = () => {
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
+
       <div className="gpt3__navbar-menu">
 
         {toggleMenu 
-        ? <RiCloseLine color="#FFF" size={27} onClick={() => setToggleMenu(true)}/> 
+        ? <RiCloseLine color="#FFF" size={27} onClick={() => setToggleMenu(false)}/> 
         : <RiMenu3Line color="#FFF" size={27} onClick={() => setToggleMenu(true)}/>}
 
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
             </div>
           </div>
         )}
-
       </div>
     </div>
   )
