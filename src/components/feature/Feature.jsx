7b2159cd5ab5d2a0bc2da769d0017c2ac5370269
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-12 15:43:21 -0700

    Finshed creating Feature component to be used in WhatGPT3 container and Completed adding content to WhatGPT3 section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+
+// Feature component will have two props or properties, Title and text that will be pass through when used in other components or containers
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div /> {/* Empty Div to be used to create a gradient bar just above the heading element h1 */}
+        <h1>{title}</h1> 
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+
+    </div>
   )
 }
 
