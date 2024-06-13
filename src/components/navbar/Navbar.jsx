096839c8c38823a3c3188a299577cfb885b674a9
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-12 21:14:21 -0700

    Finished the Possibility section of webpage. Corrected navbar anchor tags to navigate to correct sections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import logo from '../../assets/GPT-3.svg'
 const Menu = () => (
   <>
     <p> <a href="#home">Home</a> </p>
-    <p> <a href="#wgpt3">What is GPT?</a> </p>
+    <p> <a href="#whatgpt3">What is GPT?</a> </p>
     <p> <a href="#possibility">Open AI</a> </p>
     <p> <a href="#features">Case Studies</a> </p>
     <p> <a href="#blog">Library</a> </p>
