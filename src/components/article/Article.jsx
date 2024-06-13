dffa07c1db4e28da63c6bb7b8d5a2b0081e5d521
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-12 21:17:44 -0700

    Finish creating Blog section after implementing the Article component. Added mobile responsiveness to article components

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from 'react' // JSX is a syntax extension for JavaScript that lets you write HTML-like markup inside a JavaScript file.
 import './article.css'
 
-const Article = () => {
+const Article = ({ imageUrl, title, date }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blog" />
+      </div>
+      
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
